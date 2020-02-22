36c5e41f5df1b769132cfa248a3a020a757e7f53
hmsrepo1982 <54878591+hmsrepo1982@users.noreply.github.com>
2020-02-22 14:44:17 -0600

    Add files via upload
    Handling Authorization in Spring.

--- src/main/java/com/hms/spring/security/springbootsecuritysample/resource/EmployeeResource.java
+++ src/main/java/com/hms/spring/security/springbootsecuritysample/resource/EmployeeResource.java
@@ -11,5 +11,18 @@ public class EmployeeResource {
 		
 		return ("<h1>Welcome</h1>");
 	}
+	
+	@GetMapping("/user")
+	public String welcomeUser() { 
+		
+		return ("<h1>Welcome User</h1>");
+	}
+
+	
+	@GetMapping("/admin")
+	public String welcomeAdmin() { 
+		
+		return ("<h1>Welcome Admin</h1>");
+	}
 
 }
